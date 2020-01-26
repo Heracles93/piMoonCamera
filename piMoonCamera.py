f7907751aa18c45e7217023191b8c1a7eb536e27
Heracles93 <971jordan@live.fr>
2020-01-26 14:18:04 +0100

    update piMoonCamera test

--- piMoonCamera.py
+++ piMoonCamera.py
@@ -8,23 +8,42 @@ from time import sleep
 
 class piMoonCamera:
     
-    def __init__(self):
+    def __init__(self, doTest : bool = False):
         """
-        Init
+        Init the camera and test it.
+        
+        Params:
+            doTest : (bool) : test the camera if True (False by default)
         """
-        self.camera = PiCamera()
-
-    def preview(self, duration: int = 5, alpha: int = None, brightness: int = None, imageEffect : str = "none", exposure : str = "auto", whiteBalance : (str): "auto"):
+        print("Starting ...")
+        self.camera = PiCamera()       
+        if doTest:
+            self.preview()
+            print("Preview test ok")
+            sleep(2)
+            self.autoContrast()
+            print("Contrast test ok")
+            sleep(2)
+            self.autoBrightness()
+            print("Brightness test ok")
+            sleep(2)
+            self.getImage("testImage.jpg")
+            print("Image captured test ok")
+            sleep(2)
+            self.getVideo(duration=5, name="testVideo.h264")
+            print("Video captured test ok")
+        print("Camera initiated ! Done")
+    
+    def preview(self, duration: int = 5, alpha: int = None, brightness: int = None, imageEffect : str = "none", exposure : str = "auto", whiteBalance : (str) = "auto"):
         """Show the camera for <duration> secs.
 
         Params:
             duration : (int) : duration in seconds.
             alpha : (int) : set the alpha level between 0 and 255
-            brightness
+            brightness : (int) : set the brightness between 0 and 100 
             imageEffect : (str) : choose between: none (default), negative, solarize, sketch, denoise, emboss, oilpaint, hatch, gpen, pastel, watercolor, film, blur, saturation, colorswap, washedout, posterise, colorpoint, colorbalance, cartoon, deinterlace1
             exposure : (str) : choose between: off, auto (default), night, nightpreview, backlight, spotlight, sports, snow, beach, verylong, fixedfps, antishake
             whiteBalence : (str) : choose between: off, auto (default), sunlight, cloudy, shade, tungsten, fluorescent, incandescent, flash
-
         """
         if alpha:
             self.camera.start_preview(alpha=alpha)
@@ -55,9 +74,22 @@ class piMoonCamera:
         
     def getImage(self, name: str, directory: str = None, previewTime: int = 2, text: str = None, textSize: int = None, textBackgroundColor: str = None, textForegroundColor: str = None): #@TODO
         """
-        @TODO
+        Take a picture.
+        
+        Params:
+            name : (str) : ...
+            directory : (str) : ...
+            previewTime : (int) : ...
+            text : (str) : ...
+            textBackgroundColor : (str) : ...
+            textForegroundColor : (str) : ...
+            textSize : (int) : ...
         """
-        filepath = "tmp_image.jpg"
+        filepath = name
+        if directory:
+            filepath = directory + name
+        if not filepath.endswith(".jpg"):
+            filepath += ".jpg"
         self.camera.start_preview()
         if text:
             if textBackgroundColor:
@@ -78,11 +110,15 @@ class piMoonCamera:
         for i in range(nb):
             self.getImage(name=name+str(i), directory=directory, previewTime=previewTime)
     
-    def getVideo(self, duration: int, name: str):
+    def getVideo(self, duration: int, name: str, directory : str = None):
         """
         @TODO
         """
-        filepath = "tmp_video.h264"
+        filepath = name
+        if directory:
+            filepath = directory + name
+        if not filepath.endswith(".h264"):
+            filepath += ".h264"
         self.camera.start_preview()
         self.camera.start_recording(filepath)
         sleep(duration)
@@ -90,6 +126,9 @@ class piMoonCamera:
         self.camera.stop_preview()
         
     def setResolution(self, xResolution: int = 2592, yResolution: int = 1994):
+        """
+        @TODO
+        """
         self.camera.resolution = (xResolution, yResolution)
         self.framerate = 15
         self.camera.start_preview()
@@ -98,9 +137,15 @@ class piMoonCamera:
         camera.stop_preview()
         
     def setText(self, text : str):
+        """
+        @TODO
+        """
         self.camera.annotate_text = text
         
-    def autoBrightbess(self):
+    def autoBrightness(self):
+        """
+        @TODO
+        """
         self.camera.start_preview()
         for i in range(100):
             self.camera.annotate_text = "Brightness: %s" % i
@@ -109,6 +154,9 @@ class piMoonCamera:
         self.camera.stop_preview()
 
     def autoContrast(self):
+        """
+        @TODO
+        """
         self.camera.start_preview()
         for i in range(100):
             self.camera.annotate_text = "Contrast: %s" % i
@@ -117,6 +165,5 @@ class piMoonCamera:
         self.camera.stop_preview()
         
 if __name__ == "__main__":
-    print("Starting ...")
-    cam = piMoonCamera()
-    cam.preview()
+    cam = piMoonCamera(doTest=True)
+    #cam.preview(alpha=128)
